d52b831b5fedaeadf563abfa8181738a90b9d30d
huangruihao <huangruihaocst@126.com>
2016-06-03 15:12:11 +0800

    be able to get all sources.

--- public/js/bind.js
+++ public/js/bind.js
@@ -3,7 +3,39 @@
  */
 $(document).ready(function(){
     var token = $('#token').text();
-    $.ajax({
-        
+    $.get('../js/config.json', function(data){
+        var host = data['host'];
+        var auth_port = data['auth_port'];
+        var scheduler_port = data['scheduler_port'];
+        var client_port = data['client_port'];
+        $.ajax({
+            type: 'GET',
+            url: 'http://' + host + ':' + scheduler_port + '/api/v1/all_sources?token=' + token,
+            success: function(response) {
+                response = JSON.parse(response);
+                if (response['status'] == 'ok') {
+                    var sources = response['sources']; // dict
+                    for (var key in sources) {
+                        if (key == 'github' && sources.hasOwnProperty(key)) {
+                            $.ajax({
+                                type: 'GET',
+                                url: sources[key] + '/api/v1/check_registered',
+                                success: function (result) {
+                                    result = JSON.parse(result);
+                                    if (result['status'] == 'ok') {
+                                         
+                                    } else {
+                                        $('#alert').show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                } else {
+                    $('#alert').show();
+                }
+            }
+        });
     });
+
 });
