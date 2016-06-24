cceb0b651818628b28088412e0fd9b1e5f35e725
huangruihao <huangruihaocst@126.com>
2016-06-24 22:36:13 +0800

    finish the part of rss.

--- public/js/bind.js
+++ public/js/bind.js
@@ -2,6 +2,22 @@
  * Created by huangruihao on 16-5-29.
  */
 'use strict';
+
+// a helper function
+function showAlert(type, text){
+    if (type == 'success') {
+        $('#alert').hide();
+        $('#success_message').html(text);
+        $('#success').show();
+    } else if (type == 'alert') {
+        $('#success').hide();
+        $('#alert_message').html(text);
+        $('#alert').show();
+    } else {
+        // add more types here
+    }
+}
+
 function unbindLearn(source, token) {
     // if the user has bind the source
     $('#learn_status').text('已绑定');
@@ -12,19 +28,16 @@ function unbindLearn(source, token) {
         .removeClass('btn-primary')
         .click(function() {
             $.ajax({
-                type: 'DEL',
-                url: source + '/api/v1/register?token=' + token,
+                type: 'POST',
+                url: source + '/api/v1/unbind?token=' + token,
                 success: function (res) {
                     res = JSON.parse(res);
-                    if (res[status] == 'ok') {
-                        $('#alert').hide();
-                        $('#success_message').text('解绑成功');
-                        $('#success').show();
+                    if (res['status'] == 'ok') {
+                        showAlert('success', '解绑成功');
                         $('#learn_modal').modal('hide');
                         bindLearn(source, token);
                     } else {
-                        $('#message').html('未知错误<strong>(·8·)</strong>');
-                        $('#alert').show();
+                        showAlert('alert', '未知错误<strong>(·8·)</strong>');
                     }
                 }
             });
@@ -63,11 +76,9 @@ function bindLearn(source, token) {
                         unbindLearn(source, token);
                     } else {
                         if (res['reason'] == 'not a tsinghua account') {
-                            $('#message').html('这不是一个清华账号<strong>(·8·)</strong>');
-                            $('#alert').show();
+                            showAlert('alert', '这不是一个清华账号<strong>(·8·)</strong>');
                         } else {
-                            $('#message').html('未知错误<strong>(·8·)</strong>');
-                            $('#alert').show();
+                            showAlert('alert', '未知错误<strong>(·8·)</strong>');
                         }
                     }
                 }
@@ -122,10 +133,9 @@ function bindGitHub(source, token) {
                         unbindLearn(source, token);
                     } else {
                         if (res['reason'] == '') {
-                            // no supporting reason
+                            // no available reason
                         } else {
-                            $('#message').html('未知错误<strong>(·8·)</strong>');
-                            $('#alert').show();
+                            showAlert('alert', '未知错误<strong>(·8·)</strong>');
                         }
                     }
                 }
@@ -160,11 +170,9 @@ function bindVultr(source, token) {
         var username = $('#vultr_username').val();
         var v_token = $('#vultr_token').val();
         if (username == '') {
-            $('#message').html('用户名不能为空<strong>(·8·)</strong>');
-            $('#alert').show();
+            showAlert('alert', '用户名不能为空<strong>(·8·)</strong>');
         } else if (v_token == '') {
-            $('#message').html('Token不能为空<strong>(·8·)</strong>');
-            $('#alert').show();
+            showAlert('alert', 'Token不能为空<strong>(·8·)</strong>');
         } else {
             $.ajax({
                 type: 'POST',
@@ -179,15 +187,122 @@ function bindVultr(source, token) {
                         unbindLearn(source, token);
                     } else {
                         if (res['reason'] == '') {
-
+                            // no available reason
                         } else {
-                            $('#message').html(
-                                '未知错误<strong>(·8·)</strong>');
-                            $('#alert').show();
+                            showAlert('alert', '未知错误<strong>(·8·)</strong>');
                         }
                     }
                 }
             });
+        }
+    });
+}
+
+function appendSource(source, token, url, index) {
+    $('#no_subscriptions').hide();
+    var button_id = 'button_' + index;
+    var li_id = 'li_' + index;
+    var button = '<button  type="button" class="close col-sm-1" id=' + button_id +
+        '><span aria-hidden="true">&times;</span></button>';
+    var html = '<li class="row" id=' + li_id + '><a class="col-sm-10" target="_blank" href="'
+        + url + '">' + url + '</a>' + button + '</li>';
+    $('#rss_subscriptions').append(html).show();
+    $('#' + button_id).click(function () {
+        $.ajax({
+            type: 'POST',
+            url: source + '/api/v1/unsubscribe?url=' + url + '&token=' + token,
+            success: function (res) {
+                res = JSON.parse(res);
+                if (res['status'] == 'ok') {
+                    $('#' + li_id).remove();
+                    if ($('#rss_subscriptions').children().length == 0) {
+                        $(this).hide();
+                        $('#no_subscriptions').show();
+                    }
+                } else {
+                    showAlert('alert', '未知错误<strong>(·8·)</strong>');
+                }
+            }
+        });
+    });
+}
+
+function subscribe(source, token, url) {
+    $.ajax({
+        type: 'POST',
+        url: source + '/api/v1/subscribe?token=' + token + '&url=' + url,
+        success: function (res) {
+            res = JSON.parse(res);
+            if (res['status'] == 'ok') {
+                appendSource(source, token, url, $('#rss_subscriptions').children().length);
+                $('#subscribe').val('');
+            } else {
+                if (res['reason'].contains('bad URI')) {
+                    showAlert('alert', '不合法的网址<strong>(·8·)</strong>');
+                } else {
+                    showAlert('alert', '未知错误<strong>(·8·)</strong>');
+                }
+
+            }
+        }
+    });
+}
+
+function setRSSModal(source, token) {
+    $.ajax({
+        type: 'GET',
+        url: source + '/api/v1/sources?token=' + token,
+        success: function (res) {
+            res = JSON.parse(res);
+            if (res['status'] == 'ok') {
+                var sources = res['sources'];
+                if (sources.length > 0) {
+                    for(var index in sources) {
+                        if (sources.hasOwnProperty(index)) {
+                            appendSource(source, token, sources[index], index.toString());
+                        }
+                    }
+                } else {
+                    $('#rss_subscriptions').hide();
+                    $('#no_subscriptions').show();
+                }
+            } else {
+                showAlert('alert', '未知错误<strong>(·8·)</strong>');
+            }
+        }
+    });
+    $('#rss_form').submit(function (e) {
+        e.preventDefault();
+        var url = $('#subscribe').val();
+        if (url == '') {
+            showAlert('alert', '订阅网址不能为空<strong>(·8·)</strong>');
+        } else {
+            subscribe(source, token, url);
+        }
+    });
+}
+
+function manageRSSSubscribe(source, token) {
+    // check if user has RSS subscription
+    $.ajax({
+        type: 'GET',
+        url: source + '/api/v1/check_registered?token=' + token,
+        success: function (result) {
+            result = JSON.parse(result);
+            if (result['status'] == 'ok') {
+                if (result['registered']) {
+                    $('#rss_status').text('有订阅');
+                } else {
+                    $('#rss_status').text('没有订阅');
+                }
+                $('#rss_bind').text('管理订阅')
+                    .addClass('btn btn-primary btn-sm')
+                    .attr('data-toggle', 'modal')
+                    .attr('data-target', '#rss_modal');
+                setRSSModal(source, token);
+            } else {
+                showAlert('alert', '未知错误<strong>(·8·)</strong>');
+            }
         }
     });
 }
@@ -206,8 +321,7 @@ function setSource(source, token, bindSource, unbindSource) {
                     bindSource(source, token);
                 }
             } else {
-                $('#message').html('未知错误<strong>(·8·)</strong>');
-                $('#alert').show();
+                showAlert('alert', '未知错误<strong>(·8·)</strong>');
             }
         }
     });
@@ -232,14 +346,15 @@ $(document).ready(function(){
                             } else if (key == 'vultr') {
                                 setSource(sources[key], token, bindVultr, unbindVultr);
                             } else if (key == 'rss') {
-
+                                // manageRSSSubscribe(sources[key], token);
                             } else {
                                 // add more sources here
                             }
                         }
                     }
+                    manageRSSSubscribe('http://localhost:8007', token);
                 } else {
-                    $('#alert').show();
+                    showAlert('alert', '未知错误<strong>(·8·)</strong>');
                 }
             }
         });
